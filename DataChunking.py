8525bad770074dc524af80cb743028bf703b334b
Michael Kipp <michaelkipp@outlook.com>
2017-07-10 16:33:29 -0500

    Updates movies

--- DataChunking.py
+++ DataChunking.py
@@ -4,6 +4,13 @@ start_time = timeit.default_timer()
 
 lines = [[] for x in range (670000)]
 movies = {}
+conjs = {}
+
+# Create conjunction lookup
+with open('conjunctions.csv') as conjunctions:
+    for line in conjunctions:
+        line = line.split(",")
+        conjs[str(line[0])] = line[1].strip()
 
 # Create movie lookup
 with open('Data/movie_titles_metadata.txt') as movieList:
@@ -24,6 +31,13 @@ for filename in range(617):
         with open('LinesByMovie/' + movies['m' + str(filename)] + '.txt', 'w') as movie:
             movie.write('')
 
+def handle_apostraphes(str):
+    str = str.split(" ")
+    for i in range(len(str)):
+        if str[i] in conjs:
+            str[i] = conjs[str[i]]
+    return " ".join(str)
+
 # Takes lines from data and adds to list
 with open('Data/movie_lines.txt') as data:
         for line in data:
@@ -31,6 +45,11 @@ with open('Data/movie_lines.txt') as data:
             for i in range(len(singleLine)):
                 singleLine[i] = singleLine[i].decode('utf-8', errors='ignore').encode('utf-8').strip()
                 singleLine[i] = singleLine[i].lower()
+                if "'" in singleLine[i]:
+                    singleLine[i] = handle_apostraphes(singleLine[i])
+                if "u>" in singleLine[i]:
+                    singleLine[i] = singleLine[i].replace("<u>", "")
+                    singleLine[i] = singleLine[i].replace("</u>", "")
             lines[int(singleLine[0][1:])] = singleLine
 
 lines = filter(None, lines)
